703c546c425f4eb23b99b51a2cb314a79c4a004e
Leonardo Mazzeo <leomazzeo@gmail.com>
2023-12-13 14:40:15 -0300

    ya anda todo, falta compilarlo

--- tpoi/settings.py
+++ tpoi/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-6+48lz=*ous(wm-qj8uc-@jpf6ke#kytyu!8(fisnijj-+8f3*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rintrahenelcaos.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
